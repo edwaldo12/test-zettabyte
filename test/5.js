492cfca4c1fbcb98647db0b081a537e35aadf00c
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2022-04-12 20:11:20 +0700

    Update 5.js

--- test/5.js
+++ test/5.js
@@ -22,7 +22,6 @@ const students = [
   { firstName: "Tatiana", lastName: "Dickerson" },
   { firstName: "Peyton", lastName: "Gardner" },
 ];
-const groups = 3;
 
 function result(students) {
   students.sort(function (a, b) {
@@ -31,7 +30,20 @@ function result(students) {
     return 0;
   });
 
-  return students;
+  var arr = [],
+    i,
+    k;
+
+  for (i = 0, k = -1; i < students.length; i++) {
+    if (i % 2 === 0) {
+      k++;
+      arr[k] = [];
+    }
+
+    arr[k].push(students[i]);
+  }
+
+  return arr;
 }
 
-console.log(result(students, groups));
+console.log(result(students));
